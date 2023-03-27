0d248d2cad894f3529ad0c3965332419e8a26611
lesayobruce <lesayobruce419@gmail.com>
2023-03-27 10:04:01 +0300

    added validations

--- app/models/power.rb
+++ app/models/power.rb
@@ -2,4 +2,7 @@ class HeroPower < ApplicationRecord
    #hero power relations
     belongs_to :hero
     belongs_to :power
+
+    validates :description, presence: true
+    validates :description, length: { minimum: 20 }
   end
